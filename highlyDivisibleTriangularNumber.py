635fec54ba3598ee9f0c7eb26f569c801a83784a
mgruben <GrubenM@gmail.com>
2016-09-06 07:33:18 -0500

    Implement getDivisor function

--- highlyDivisibleTriangularNumber.py
+++ highlyDivisibleTriangularNumber.py
@@ -5,3 +5,15 @@ def genTriangular():
         yield n
         n += m
         m += 1
+
+def getDivisors(n):
+    divisors = []
+    if n == 1:
+        divisors.append(1)
+    else:
+        divisors.append(1)
+        divisors.append(n)
+    for i in range(2, (n // 2) + 1):
+        if n % i == 0:
+            divisors.append(i)
+    return sorted(divisors)
