a65a9608cdd5e1e807a39a7c5e66638207d4bfff
mgruben <GrubenM@gmail.com>
2016-09-06 18:42:20 -0500

    Implement countDivisors in c++

--- highlyDivisibleTriangularNumber.cpp
+++ highlyDivisibleTriangularNumber.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 using namespace std;
 
-
+int countDivisors(int n) {
+    c = 0;
+    if (n == 1) c++;    // 1 is a factor
+    else c += 2;        // 1 and n are factors
+    if (n / sqrt(n) == sqrt(n)) c++; // sqrt(n) is a factor
+    for (int i = 2; (int) floor(sqrt(n)); i++) {
+        if (n % i == 0) c+= 2; // i and n / i are factors
+    }
+    return c;
+}
 
 int main() {
     return 0;
