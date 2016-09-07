0ec384de822dd6f242f592114e190c4e67b10d2d
mgruben <GrubenM@gmail.com>
2016-09-06 19:38:43 -0500

    Use a more-terse countDivisors construction

--- highlyDivisibleTriangularNumber.cpp
+++ highlyDivisibleTriangularNumber.cpp
@@ -5,12 +5,11 @@ using namespace std;
 
 int countDivisors(int n) {
     int c = 0;
-    if (n == 1) return 1;    // 1 is a factor
-    else c += 2;        // 1 and n are factors
-    int sq = ceil(sqrt(n));
-    if ((n / sq) == sq) c++; // sqrt(n) is a factor
-    for (int i = 2; i < sq; i++) {
-        if (n % i == 0) c+= 2; // i and n / i are factors
+    for (int i = 1; i * i <= n; i++) {
+        if (n % i == 0) {
+            c+= 2; // i and n / i are factors
+            if (n / i == i) c--;
+        }
     }
     return c;
 }
@@ -19,11 +18,11 @@ int main() {
     int maxDivs = 500;
     int divs = 0;
     int n;
-    for (n = 2; divs <= maxDivs; n++) {
+    for (n = 10; divs <= maxDivs; n++) {
         if (n % 2 == 0) divs = countDivisors(n / 2) * countDivisors(n + 1);
         else divs = countDivisors(n) * countDivisors((n + 1) / 2);
+        cout << n << " " << divs << endl;
     }
-    cout << n << " " << divs << endl;
     cout << (n * (n + 1) / 2) << endl;
     return 0;
 }
