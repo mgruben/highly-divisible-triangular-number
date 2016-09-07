4467edb9a1b33981f6c6b07006e9726851c6f57f
mgruben <GrubenM@gmail.com>
2016-09-06 19:21:54 -0500

    Fix countDivisors in c++

--- highlyDivisibleTriangularNumber.cpp
+++ highlyDivisibleTriangularNumber.cpp
@@ -1,17 +1,29 @@
 #include <iostream>
+#include <math.h>
+
 using namespace std;
 
 int countDivisors(int n) {
-    c = 0;
-    if (n == 1) c++;    // 1 is a factor
+    int c = 0;
+    if (n == 1) return 1;    // 1 is a factor
     else c += 2;        // 1 and n are factors
-    if (n / sqrt(n) == sqrt(n)) c++; // sqrt(n) is a factor
-    for (int i = 2; (int) floor(sqrt(n)); i++) {
+    int sq = ceil(sqrt(n));
+    if ((n / sq) == sq) c++; // sqrt(n) is a factor
+    for (int i = 2; i < sq; i++) {
         if (n % i == 0) c+= 2; // i and n / i are factors
     }
     return c;
 }
 
 int main() {
+    int maxDivs = 500;
+    int divs = 0;
+    int n;
+    for (n = 2; divs < maxDivs; n++) {
+        if (n % 2 == 0) divs = countDivisors(n / 2) * countDivisors(n + 1);
+        else divs = countDivisors(n) * countDivisors((n + 1) / 2);
+    }
+    cout << n << " " << divs << endl;
+    cout << (n * (n + 1) / 2) << endl;
     return 0;
 }
